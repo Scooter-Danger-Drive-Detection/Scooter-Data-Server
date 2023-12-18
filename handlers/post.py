e425af22526137b9c6669963e1a793291d421b74
Markusssian <mareshkakrakazabr@gmail.com>
2023-12-18 21:54:08 +0300

    /StartSession fix

--- handlers/post.py
+++ handlers/post.py
@@ -33,7 +33,8 @@ def get_frames_count():
 @app.route("/StartSession", methods=["POST"])
 def start_session():
     data = request.json
-    return json.dumps({"SessionID": session_table.add_session(Session(data))})
+    return json.dumps({"SessionID": session_table.add_session(Session(data.get("SessionID"), data.get("UserID"),
+                                                                      get_ride_mode_by_key(data.get("RideMode"))))})
 
 
 @app.route("/SaveSession", methods=["POST"])
