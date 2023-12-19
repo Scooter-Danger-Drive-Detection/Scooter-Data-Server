f2811f1843d55d00863901140dc87848ec757b70
Markusssian <mareshkakrakazabr@gmail.com>
2023-12-19 12:16:13 +0300

    TestRideMode

--- models/ride_mode.py
+++ models/ride_mode.py
@@ -18,10 +18,17 @@ class UnsafeRideMode(RideMode):
             self.key = 2
 
 
+class TestRideMode(RideMode):
+    def __init__(self):
+        super(TestRideMode, self).__init__()
+        self.key = 3
+
+
 def get_ride_mode_by_key(key: int):
     ride_modes = {
         0: SafeRideMode(),
         1: UnsafeRideMode(alone=True),
-        2: UnsafeRideMode(alone=False)
+        2: UnsafeRideMode(alone=False),
+        3: TestRideMode(),
     }
     return ride_modes[key]
